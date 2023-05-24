34285968abd35894f54847f3a69b11911ba8f355
sebbyapudo <sebbyapudo@gmail.com>
2023-05-24 22:06:02 +0300

    Set up a getElement function that double checks a selector's classname and throws an exception if there isn't a match

--- js/app.js
+++ js/app.js
@@ -1,1 +1,5 @@
-
+const getElement = (selector) =>{
+  const element = document.querySelector(selector);
+  if(element) return element
+  throw(`Please double check your class names, there is no ${selector} class`)
+}
